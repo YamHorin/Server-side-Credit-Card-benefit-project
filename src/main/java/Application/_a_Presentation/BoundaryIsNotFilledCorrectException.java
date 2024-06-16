121a47506092fb9e0ea56ef1698e3f9076e8d0d4
Yam.Horin <yam.horin@s.afeka.ac.il>
2024-06-16 14:26:48 +0300

    yAM 16.6 TEST branch

--- src/main/java/Application/_a_Presentation/BoundaryIsNotFilledCorrectException.java
+++ src/main/java/Application/_a_Presentation/BoundaryIsNotFilledCorrectException.java
@@ -3,6 +3,8 @@ package Application._a_Presentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 
 public class BoundaryIsNotFilledCorrectException  extends RuntimeException{
